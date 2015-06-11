dac6147e318324af21f9abec43ffab63e3919f7b
Laurent da <ephyto@gmail.com>
2015-06-11 10:03:58 +0200

    complete1

---
+++ complete.R
@@ -0,0 +1,25 @@
+complete <- function(directory, id = 1:332) {
+	filepath <- paste("C:/Users/ephyto/Google Drive/R - coursera", directory, sep="/")
+	allfiles <- as.character(list.files(filepath))
+	specdatapath <- paste(filepath, allfiles, sep="/")
+
+	
+	idx <- c()
+	nobs <- c()
+	y <- data.frame()
+	xf <- c()
+	x <- 0
+	for(i in id){
+		x <- x + 1
+		xf = append(xf, x)
+		idx = append(idx, i)
+		y <- read.csv(specdatapath[i], sep=",")
+		y <- y[complete.cases(y),]
+		nobs <- append(nobs, nrow(y))
+	}
+	dataset = cbind(xf, idx, nobs)
+	colnames(dataset) <- c("", "id", "nobs")
+dataset
+	#print(idx)
+	#print(nobs)
+}
